cdd77ac21418e433dc9617c587037df12fe37727
Omeed Habibelahian <habibelo@oregonstate.edu>
2019-07-31 09:30:09 -0700

    Calculate number of occurrences of each value pair (1-to-1 attr only)

--- datacleaner/helpers.py
+++ datacleaner/helpers.py
@@ -1,25 +1,62 @@
 from random import randint
+from tqdm import tqdm
 
 def parseCSV(csv_file):
     header = list()
+    relationships = dict()
     csv_data = list()
+
     data = csv_file.read().decode('utf-8')
     lines = data.split('\n')
-    i = 0
-    for line in lines:
+    count = 0
+    maxOccurence = 1
+    for line in tqdm(lines):
         fields = line.split(',')
-        if i == 0:
+        if count == 0:
             for field in fields:
-
                 header.append(field)
         else:
             data_line = list()
-            for val in fields:
+            for i in range(0, len(fields)):
                 field = dict()
-                field.update({'cellValue': val})
-                field.update({'dirtiness': randint(0,10)})
+                field.update({'cellValue': fields[i]})
+                field.update({'dirtiness': -1})
                 data_line.append(field)
+                for j in range(0, len(fields)):
+                    if i != j:
+                        if (header[i], fields[i]) not in relationships.keys() or (header[j], fields[j]) not in relationships[(header[i], fields[i])].keys():
+                            relationships[(header[i], fields[i])] = dict()
+                            relationships[(header[i], fields[i])][(header[j], fields[j])] = 1
+                        else:
+                            relationships[(header[i], fields[i])][(header[j], fields[j])] += 1
+                            if relationships[(header[i], fields[i])][(header[j], fields[j])] > maxOccurence:
+                                maxOccurence = relationships[(header[i], fields[i])][(header[j], fields[j])]
             csv_data.append(data_line)
-        i += 1
-        print(i)
-    return header, csv_data
+        count += 1
+    print(relationships)
+    return header, csv_data, relationships, maxOccurence
+
+#def parseCSV(csv_file):
+#    header = list()
+#    csv_data = list()
+#    data = csv_file.read().decode('utf-8')
+#    lines = data.split('\n')
+#    i = 0
+#    for line in lines:
+#        fields = line.split(',')
+#        if i == 0:
+#            for field in fields:
+#
+#                header.append(field)
+#        else:
+#            data_line = list()
+#            for val in fields:
+#                field = dict()
+#                field.update({'cellValue': val})
+#                #field.update({'dirtiness': randint(0,10)})
+#                field.update({'dirtiness': -1})
+#                data_line.append(field)
+#            csv_data.append(data_line)
+#        i += 1
+#        print(i)
+#    return header, csv_data
--- datacleaner/views.py
+++ datacleaner/views.py
@@ -13,9 +13,9 @@ def results(request):
 	if request.method == 'POST':
 		form = UploadFileForm(request.POST, request.FILES)
 		if len(request.FILES) > 0 and request.FILES['data'].name.endswith('.csv'):
-			header, csv_data = parseCSV(request.FILES['data'])
+			header, csv_data, relationships, maxOccurence = parseCSV(request.FILES['data'])
 			sent_data = sample(csv_data, 10)
-			return render(request, 'datacleaner/results.html', {'header': header, 'data': sent_data, 'fullData': csv_data})
+			return render(request, 'datacleaner/results.html', {'header': header, 'data': sent_data, 'fullData': csv_data, 'maxOccurence': maxOccurence})
 		else:
 			return render(request, 'datacleaner/index.html')
 	return render(request, 'datacleaner/index.html')
