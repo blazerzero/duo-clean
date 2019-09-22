7129f8085ee93a4838bc9a777301b061ea2a4f21
Omeed Habibelahian <habibelo@oregonstate.edu>
2019-09-21 18:54:32 -0700

    More work on implementing welcome page

--- react-vc-app/src/screens/Import.js
+++ react-vc-app/src/screens/Import.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { createRef, Component } from 'react';
 import { Redirect, withRouter } from 'react-router-dom';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import {
@@ -55,32 +55,88 @@ class Import extends Component {
     }
   };
 
+  constructor(props) {
+    super(props);
+  }
+
+  _handleGetStartedClick() {
+    document.getElementById('welcomeDiv').style.display = 'none';
+    document.getElementById('importDiv').style.display = 'block';
+  }
+
   render() {
     return (
       <Route render={({ history }) => (
-        <div className='body-section home'>
-          <Form noValidate encType='multipart/form-data'>
-            <Row className='content-centered'>
-              <div className='upload-btn-wrapper'>
+        <div className='home'>
+          <Row className='content-centered'>
+            <div className='home-header'>
+              <span className='home-title'>VarClean</span>
+              <p className='home-subtitle'>Intelligently clean your data.</p>
+            </div>
+          </Row>
+          <div className='body-section'>
+            <div id='welcomeDiv' style={{display: 'block'}}>
+              <Row className='content-centered'>
                 <Button
                   variant='info'
-                  className='btn-round'
+                  className='btn-round left'
                   size='lg'>
-                  Upload a file
+                  LEARN MORE
                 </Button>
-                <input type='file' name='myfile' />
-              </div>
-            </Row>
-            <Row className='content-centered'>
-              <Button
-                variant='primary'
-                size='lg'
-                className='btn-round'
-                onClick={() => this._handleSubmit(history)}>
-                Import
-              </Button>
-            </Row>
-          </Form>
+                <Button
+                  variant='primary'
+                  className='btn-round right'
+                  size='lg'
+                  onClick={this._handleGetStartedClick}>
+                  GET STARTED
+                </Button>
+              </Row>
+            </div>
+            <div id='importDiv' style={{display: 'none'}}>
+              <Form noValidate encType='multipart/form-data'>
+                <Row className='content-centered'>
+                  <Button
+                    variant='info'
+                    className='btn-round left'
+                    size='lg'
+                    onClick={() => document.getElementById('fileUploaderHandler').click()}>
+                    CHOOSE FILE
+                  </Button>
+                  <Button
+                    variant='light'
+                    className='btn-round right filename-area'
+                    size='lg'>
+                    {this.state.importedFile !== null ? this.state.importedFile.name : 'No file chosen'}
+                  </Button>
+                  <Form.Control
+                    type='file'
+                    placeholder='Choose a file'
+                    accept='.csv'
+                    className='default-file-upload'
+                    id='fileUploaderHandler'
+                    onChange={(e) => {
+                      console.log(e.target.files);
+                      this.setState({ importedFile: e.target.files[0] }, () => {
+                        console.log(this.state);
+                        document.getElementById('findRulesBtn').style.cursor = 'pointer';
+                      });
+                    }}/>
+                </Row>
+                <Row className='content-centered'>
+                  <Button
+                    variant='primary'
+                    size='lg'
+                    className='btn-round'
+                    style={{cursor: 'default'}}
+                    id='findRulesBtn'
+                    disabled={this.state.importedFile === null}
+                    onClick={() => this._handleSubmit(history)}>
+                    FIND RULES
+                  </Button>
+                </Row>
+              </Form>
+            </div>
+          </div>
         </div>
       )} />
 
