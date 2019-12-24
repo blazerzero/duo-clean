ccd953f5e3d990194bec55cde5f5d630ebae4880
Omeed Habibelahian via fw-charm <habibelo@oregonstate.edu>
2019-12-23 21:41:46 -0800

    Remove CMake configs from repo + add my csvs + fix syntax bug in main

--- vc-server/xplode-master/main.cpp
+++ vc-server/xplode-master/main.cpp
@@ -6,11 +6,11 @@
 
 int main(int argc, char *argv[]) {
     if (argc != 5) {
-        std::cout << "Usage: ./XPlode dirtyfile cleanfile epsilon delta" << std::endl;
+        std::cout << "Usage: ./CTane dirtyfile cleanfile epsilon delta" << std::endl;
 		std::cout << "\t where dirtyfile is a dirty dataset in csv format," << std::endl;
 		std::cout << "\t cleanfile is a (partially) cleaned version of this dataset," << std::endl;
-		std::cout << "\t and epsilon and delta are confidence and support parameters for the underlying rule mining algorithm"
-        std::cout << "\t For Example: ./XPlode abalone-dirty.csv abalone-cleaned.csv 0.1 100" << std::endl;
+		std::cout << "\t and epsilon and delta are confidence and support parameters for the underlying rule mining algorithm";
+        std::cout << "\t For Example: ./CTane abalone-dirty.csv abalone-cleaned.csv 0.1 100" << std::endl;
     }
     else {
         std::ifstream dirtyFile(argv[1]);
@@ -20,7 +20,7 @@ int main(int argc, char *argv[]) {
         double epsilon = atof(argv[3]);
         int delta = atoi(argv[4]);
         XPlode xplode(dirtyDb, cleanDb);
-        CFD expl = xplode.explain(delta, 1.0-epsilon);
+        CFD expl = xplode.postExplain(delta, 1.0-epsilon);
         std::cout << "Best Explanation: " << std::endl;
         Output::printCFD(expl, cleanDb);
     }
