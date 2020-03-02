fe961e8f11c380a9e912aab8d59949007306a190
Omeed Habibelahian via fw-charm <habibelo@oregonstate.edu>
2020-03-01 16:57:03 -0800

    On fw-charm server now, just have to config to run here

--- vc-venv/bin/django-admin.py
+++ vc-venv/bin/django-admin.py
@@ -1,4 +1,4 @@
-#!/Users/family/Desktop/gradschool/ms_thesis/var-clean-app/vc-venv/bin/python3
+#!/home/habibelo/Desktop/var-clean-app/vc-venv/bin/python3
 from django.core import management
 
 if __name__ == "__main__":
