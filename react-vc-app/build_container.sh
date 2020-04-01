145cb41520c0172fba17c40793a0f57a620d3243
Omeed Habibelahian <habibelo@oregonstate.edu>
2020-04-01 04:50:46 +0000

    Frontend containerized

--- react-vc-app/build_container.sh
+++ react-vc-app/build_container.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker image build -t duoclean-front .
-docker container run -p 3000:3000 -d duoclean-front
+docker image build --no-cache -t duoclean-front .
+docker container run -p 8000:8000 -d duoclean-front
--- react-vc-app/src/screens/Results.js
+++ react-vc-app/src/screens/Results.js
@@ -76,7 +76,7 @@ class Results extends Component {
           const url = window.URL.createObjectURL(new Blob([response.data]));
           const link = document.createElement('a');
           link.href = url;
-          link.setAttribute('download', 'charm_cleaned.zip');
+          link.setAttribute('download', 'charm_cleaned.csv');
           document.body.appendChild(link);
           link.click();
           document.body.removeChild(link);
