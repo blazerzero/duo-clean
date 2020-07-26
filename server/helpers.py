b8f6a7f1c8a8a697b5824fa1faf5d8099083927d
Omeed Habibelahian <habibelo@oregonstate.edu>
2020-07-26 08:30:27 -0700

    Update helpers.py

--- server/helpers.py
+++ server/helpers.py
@@ -66,7 +66,8 @@ def saveNoiseFeedback(data, feedback, project_id, current_iter):
     with open('./store/' + project_id + '/project_info.json', 'r') as f:
         project_info = json.load(f)
         clean_dataset = pd.read_csv(project_info['scenario']['clean_dataset'], keep_default_na=False)
-        
+    
+    # scoring function: score based on number of true errors correctly identified
     score = 0
     for idx in data.index:
         for col in data.columns:
