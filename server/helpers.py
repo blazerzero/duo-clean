02f0020aab2757f08298625c1610734cf8da3e04
OMEED-MACBOOK <omeed.habibelahian@gmail.com>
2020-10-11 15:52:03 -0700

    Update calculation of complexity bias

--- server/helpers.py
+++ server/helpers.py
@@ -10,8 +10,9 @@ import csv
 import pickle
 import math
 import statistics
-from scipy.special import lambertw
 from collections import Counter
+
+from analyze import sHeuristicSetRelation
 
 class CellFeedback(object):
     def __init__(self, iter_num, marked, elapsed_time):
@@ -278,11 +279,11 @@ def reinforceTuplesBasedOnDependencies(data, project_id, current_iter, is_new_fe
 
     print('*** Metadata objects loaded ***')
 
+    all_cfds = cfd_metadata.keys()
     for cfd, cfd_m in cfd_metadata.items():
         # Bias towards simpler rules
         lhs = cfd.split(' => ')[0][1:-1].split(', ')
-        num_attributes = len(lhs)
-        complexity_bias = 1 / num_attributes
+        complexity_bias = sHeuristicSetRelation(cfd, all_cfds)
         print('*** Complexity bias calculated ***')
 
         # System's weighted prior on rule confidence
