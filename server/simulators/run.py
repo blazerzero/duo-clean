d8bc8a4560958f871ef233baf567be09a9a43659
Omeed Habibelahian <omeed.habibelahian@gmail.com>
2020-12-18 16:09:27 -0800

    Convert bayesian from .ts to .py (behavior still WIP) and update run.py accordingly

--- server/simulators/run.py
+++ server/simulators/run.py
@@ -1,4 +1,6 @@
 import subprocess as sp
+import contextlib
+import bayesian
 
 scenarios = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
 simulators = ['oracle', 'informed-bayesian', 'uninformed-bayesian']
@@ -13,19 +15,23 @@ for sim in simulators:
 
     for s in scenarios:
         if sim == 'informed-bayesian':
-            f_out = open('./results/informed-bayesian/' + sim + '-sim-out-' + str(s) + '.txt', 'w')
-            f_err = open('./results/informed-bayesian/' + sim + '-sim-err-' + str(s) + '.txt', 'w')
-            cmd = 'yarn run bayesian ' + str(s) + ' informed'
+            with open('./results/informed-bayesian/' + sim + '-sim-out-' + str(s) + '.txt', 'w') as f_out:
+                with open('./results/informed-bayesian/' + sim + '-sim-err-' + str(s) + '.txt', 'w') as f_err:
+                    with contextlib.redirect_stdout(f_out):
+                        with contextlib.redirect_stderr(f_err):
+                            bayesian.run(s, 'informed')
         elif sim == 'uninformed-bayesian':
-            f_out = open('./results/uninformed-bayesian/' + sim + '-sim-out-' + str(s) + '.txt', 'w')
-            f_err = open('./results/uninformed-bayesian/' + sim + '-sim-err-' + str(s) + '.txt', 'w')
-            cmd = 'yarn run bayesian ' + str(s) + ' uninformed'
+            with open('./results/uninformed-bayesian/' + sim + '-sim-out-' + str(s) + '.txt', 'w') as f_out:
+                with open('./results/uninformed-bayesian/' + sim + '-sim-err-' + str(s) + '.txt', 'w') as f_err:
+                    with contextlib.redirect_stdout(f_out):
+                        with contextlib.redirect_stderr(f_err):
+                            bayesian.run(s, 'uninformed')
         else:
             f_out = open('./results/oracle/' + sim + '-sim-out' + str(s) + '.txt', 'w')
             f_err = open('./results/oracle/' + sim + '-sim-err' + str(s) + '.txt', 'w')
             cmd = 'yarn run ' + sim + ' ' + str(s)
-        p = sp.Popen(cmd, stdout=f_out, stderr=f_err, shell=True)
-        output = p.communicate()[0]
+            p = sp.Popen(cmd, stdout=f_out, stderr=f_err, shell=True)
+            output = p.communicate()[0]
     
     f_out.close()
     f_err.close()
