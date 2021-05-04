83ecda299e2fd80995e8bfe93f3c4303085f5c52
Omeed Habibelahian <omeed.habibelahian@gmail.com>
2021-05-04 12:39:18 -0700

    Update eval_h.py

--- server/eval_h.py
+++ server/eval_h.py
@@ -401,7 +401,7 @@ def eval_h_grouped(group_type, run_type, id):
         cumulative_precision, cumulative_recall = study_metrics['cumulative_precision'], study_metrics['cumulative_recall']
         cumulative_precision_noover, cumulative_recall_noover = study_metrics['cumulative_precision_noover'], study_metrics['cumulative_recall_noover']
 
-        if len(user_h_history) > 1:
+        if len(user_h_history) > 2:
             console.print('\nSEPARATE USER\n')
             console.log(user_h_conf_history)
             ax1.plot([i['iter_num'] for i in user_h_history], user_h_conf_history)
