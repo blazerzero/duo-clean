abffdc4c7c081e108eb87a06d6fb836109b069c2
Omeed Habibelahian <omeed.habibelahian@gmail.com>
2021-05-05 12:20:27 -0700

    Update helpers.py

--- server/helpers.py
+++ server/helpers.py
@@ -893,7 +893,9 @@ def deriveStats(interaction_metadata, fd_metadata, h_space, study_metrics, dirty
             fd_m['beta_history'].append({ 'iter_num': i, 'value': fd_m['beta'], 'elapsed_time': elapsed_time })
             fd_m['conf_history'].append({ 'iter_num': i, 'value': fd_m['conf'], 'elapsed_time': elapsed_time })
 
-            if fd != max_h and (max_h == 'Not Sure' or fd_m['conf'] > fd_metadata[max_h]['conf_history'][-1]['value']):
+            formatted_max_h = next(f['cfd'] for f in fd_metadata.values() if set(f.split(' => ')[0][1:-1].split(', ')) == max_h_lhs and set(f.split(' => ')[1].split(', ')) == max_h_rhs)
+
+            if fd != max_h and (max_h == 'Not Sure' or fd_m['conf'] > fd_metadata[formatted_max_h]['conf_history'][-1]['value']):
                 max_h = fd
 
             if fd != target_fd:
