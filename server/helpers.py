23b5d64cff6dd3d005c34603ad2cd4da3a497634
Omeed Habibelahian <omeed.habibelahian@gmail.com>
2021-05-10 12:12:11 -0700

    Reformat FD if it's in the h space in a different attr ordering

--- server/helpers.py
+++ server/helpers.py
@@ -761,8 +761,15 @@ def deriveStats(interaction_metadata, fd_metadata, h_space, study_metrics, dirty
 
     max_h = user_hypothesis_history[0]['value'][0]
     for h in h_space:
+        lhs = set(h['cfd'].split(' => ')[0][1:-1].split(', '))
+        rhs = set(h['cfd'].split(' => ')[1].split(', '))
         # if h['cfd'] not in fd_metadata.keys():
         #     continue
+        if max_h_lhs == lhs and max_h_rhs == rhs:
+            max_h = h['cfd']
+
+        max_h_lhs = max_h.split(' => ')[0][1:-1].split(', ')
+        max_h_rhs = max_h.split(' => ')[1].split(', ')
 
         mu = h['conf'] if h['cfd'] != max_h else 1
         variance = 0.0025
